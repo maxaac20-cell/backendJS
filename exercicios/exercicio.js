a45581789bb2b0e89302d810afe518c9ed1e1d2f
maxaac20-cell <maxaac20@gmail.com>
2025-10-08 21:24:27 -0300

    exercicio vscode

---
+++ exercicios/exercicio.js
@@ -0,0 +1,8 @@
+let mensagem = "Jvascipt é muito legal!";
+let nome = 'Maxwell';
+let idade = 18;
+let cidade = 'Ouro Preto';
+let frase = `Meu nome é ${nome}\n\t tenho ${idade} anos e moro em ${cidade}`;
+console.log(frase);
+console.log(mensagem.toUpperCase());
+console.log(mensagem.toLowerCase());    
